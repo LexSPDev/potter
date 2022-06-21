db3e13961ff9062f0e0df4f874e6b7c750d59b34
LexFitCode <alejandro.roqve@gmail.com>
2022-06-21 10:30:32 -0500

    carpetas

---
+++ src/components/header/header.tsx
@@ -0,0 +1,9 @@
+import letras  from "../../assets/img/letras.png"
+
+export const Header = () => {
+  return (
+    <div className="imgWrapper">
+      <img src={letras} alt="Harry Potter" />
+    </div>
+  )
+}
---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import { Header } from "../components/header/header"
+import { Select } from "../components/select/select"
+export const Home = () => {
+  return (
+    <div className="mainWrapper">
+        <Header></Header>
+        <Select></Select>
+        
+    </div>
+  )
+}
