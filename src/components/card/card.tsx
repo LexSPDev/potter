44d93a2466ecac5d37994bfb0e2c406bb6402112
LexFitCode <alejandro.roqve@gmail.com>
2022-06-21 13:30:58 -0500

    card mobile

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -59,6 +59,35 @@ export const Card = () => {
         </h4>
       </div>
     </div>
+    <div className="cardProfile dead">
+      <div className="cardPhoto ravenclaw">
+        <img src="http://hp-api.herokuapp.com/images/filch.jpg" alt="nombre" />
+      </div>
+      <div className="cardData">
+        <h2>Harry Potter</h2>
+        <div className="aliveWrapper">
+          <h3>VIVO / ESTUDIANTE </h3>
+          <i className="ph-bookmark-simple-fill"></i>
+        </div>
+        <h2>Harry Potter</h2>
+        <h4>
+          <span>Cumpleaños: </span>
+          30-07-1980
+        </h4>
+        <h4>
+          <span>Género: </span>
+          Male
+        </h4>
+        <h4>
+          <span>Color de ojos: </span>
+          Green
+        </h4>
+        <h4>
+          <span>Color de pelo: </span>
+          black
+        </h4>
+      </div>
+    </div>
   </>
   )
 }
