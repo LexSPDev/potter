2d8019a4335d5083ecfe5de56a6ae48c27303011
LexFitCode <alejandro.roqve@gmail.com>
2022-06-21 13:27:06 -0500

    card mobile

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,12 +1,16 @@
 export const Card = () => {
   return (
-<>
-<div className="cardProfile">
+  <>
+    <div className="cardProfile alive">
       <div className="cardPhoto gryffindor">
         <img src="http://hp-api.herokuapp.com/images/filch.jpg" alt="nombre" />
       </div>
       <div className="cardData">
-        <h3>VIVO / ESTUDIANTE</h3>
+        <h2>Harry Potter</h2>
+        <div className="aliveWrapper">
+          <h3>VIVO / ESTUDIANTE </h3>
+          <i className="ph-bookmark-simple"></i>
+        </div>
         <h2>Harry Potter</h2>
         <h4>
           <span>Cumpleaños: </span>
@@ -26,81 +30,35 @@ export const Card = () => {
         </h4>
       </div>
     </div>
-    <div className="cardProfile">
-      <div className="cardPhoto slytherin">
-        <img src="http://hp-api.herokuapp.com/images/filch.jpg" alt="nombre" />
-      </div>
-      <div className="cardData">
-        <h3>VIVO / ESTUDIANTE</h3>
-        <h2>Harry Potter</h2>
-        <h4>
-          <span>Cumpleaños :</span>
-          30-07-1980
-        </h4>
-        <h4>
-          <span>Género :</span>
-          Male
-        </h4>
-        <h4>
-          <span>Color de ojos :</span>
-          Green
-        </h4>
-        <h4>
-          <span>Color de pelo :</span>
-          black
-        </h4>
-      </div>
-    </div>
-    <div className="cardProfile">
-      <div className="cardPhoto ravenclaw">
-        <img src="http://hp-api.herokuapp.com/images/filch.jpg" alt="nombre" />
-      </div>
-      <div className="cardData">
-        <h3>VIVO / ESTUDIANTE</h3>
-        <h2>Harry Potter</h2>
-        <h4>
-          <span>Cumpleaños :</span>
-          30-07-1980
-        </h4>
-        <h4>
-          <span>Género :</span>
-          Male
-        </h4>
-        <h4>
-          <span>Color de ojos :</span>
-          Green
-        </h4>
-        <h4>
-          <span>Color de pelo :</span>
-          black
-        </h4>
-      </div>
-    </div>
-    <div className="cardProfile">
+    <div className="cardProfile alive">
       <div className="cardPhoto hufflepuff">
         <img src="http://hp-api.herokuapp.com/images/filch.jpg" alt="nombre" />
       </div>
       <div className="cardData">
-        <h3>VIVO / ESTUDIANTE</h3>
+        <h2>Harry Potter</h2>
+        <div className="aliveWrapper">
+          <h3>VIVO / ESTUDIANTE </h3>
+          <i className="ph-bookmark-simple-fill"></i>
+        </div>
         <h2>Harry Potter</h2>
         <h4>
-          <span>Cumpleaños :</span>
+          <span>Cumpleaños: </span>
           30-07-1980
         </h4>
         <h4>
-          <span>Género :</span>
+          <span>Género: </span>
           Male
         </h4>
         <h4>
-          <span>Color de ojos :</span>
+          <span>Color de ojos: </span>
           Green
         </h4>
         <h4>
-          <span>Color de pelo :</span>
+          <span>Color de pelo: </span>
           black
         </h4>
       </div>
     </div>
-</>
+  </>
   )
 }
